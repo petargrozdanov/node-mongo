50a38237f08058e789b2b2833ef2407a800b2ff2
Petar <49168149+petargrozdanov@users.noreply.github.com>
2019-11-06 22:04:58 +0100

    node-mongo

---
+++ index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://dev:dev123!cluster0-fwwvy.mongodb.net:27017/videoteka', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(res => {
+        console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+const filmovi = mongoose.model(
+    'Filmovi', 
+    { 
+       ime: String,
+       godina: String,
+       zhanr: String,
+       rezhija: String,
+       oscar: String,
+       akreti: [String]
+
+    }
+);
+
+filmovi.find((err, data) => {
+    if(err){
+        console.log('ERROR!')
+    }
+    console.log(data);
+});
+console.log('end!');  
